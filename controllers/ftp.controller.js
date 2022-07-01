5199aa1555b5524dfd8cac53b66797d5602ba4ed
LuckyZacha <zachatatem@gmail.com>
2022-07-01 15:14:43 +0800

    Fix: login with one user

--- controllers/ftp.controller.js
+++ controllers/ftp.controller.js
@@ -1,61 +1,61 @@
 /** @format */
-const CloudinfoModel = require("../models/cloudInfoModel");
-var { sendResponse } = require("./ControllerHepler");
+const CloudinfoModel = require('../models/cloudInfoModel')
+var {sendResponse} = require('./ControllerHepler')
 
-const fs = require("fs");
-const archiver = require("archiver");
+const fs = require('fs')
+const archiver = require('archiver')
 
-const path = require("path");
+const path = require('path')
 
 const getFtpService = async (req) => {
-  const ftpService = req.app.get("ftp-service");
+  const ftpService = req.app.get('ftp-service')
 
-  const username = req.body.username || process.env.FTP_USER;
-  const host = req.body.host || process.env.FTP_SERVER;
-  const cloudInfo = await CloudinfoModel.findOne({
-    username: username,
-    host: host,
-  });
-  if (typeof cloudInfos !== undefined && cloudInfo !== null) {
-    await ftpService.loginServer(
-      cloudInfo.url,
-      cloudInfo.username,
-      cloudInfo.password,
-    );
-    return ftpService;
+  const username = req.body.username || process.env.FTP_USER
+  const host = req.body.host || process.env.FTP_SERVER
+  let cloudInfo
+  try {
+    cloudInfo = await CloudinfoModel.findOne({
+      username: username,
+      host: host,
+    })
+  } catch (e) {
+    console.log(e)
+    throw 'service database error'
   }
-  throw "cannot login to ftp server";
-};
+  if (typeof cloudInfo !== undefined && cloudInfo !== null) {
+    await ftpService.getService(cloudInfo.url, cloudInfo.username, cloudInfo.password)
+    return ftpService
+  }
+  throw 'cannot login to ftp server dut to database error'
+}
 
 const archive = async (path, tmpPath, name) => {
-  const fileName = path + name + ".zip";
-  const output = fs.createWriteStream(fileName);
-  const archive = archiver("zip", {
-    zlib: { level: 9 },
-  });
-  output.on("close", function () {
-    console.log(archive.pointer() + " total bytes");
-    console.log(
-      "archiver has been finalized and the output file descriptor has closed.",
-    );
-  });
-  output.on("end", function () {
-    console.log("Data has been drained");
-  });
-  archive.on("warning", function (err) {
-    if (err.code === "ENOENT") {
-      console.log(err.code);
+  const fileName = path + name + '.zip'
+  const output = fs.createWriteStream(fileName)
+  const archive = archiver('zip', {
+    zlib: {level: 9},
+  })
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes')
+    console.log('archiver has been finalized and the output file descriptor has closed.')
+  })
+  output.on('end', function () {
+    console.log('Data has been drained')
+  })
+  archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+      console.log(err.code)
     } else {
-      throw err;
+      throw err
     }
-  });
-  archive.on("error", function (err) {
-    throw err;
-  });
-  archive.pipe(output);
-  archive.directory(tmpPath, false);
-  archive.finalize();
-};
+  })
+  archive.on('error', function (err) {
+    throw err
+  })
+  archive.pipe(output)
+  archive.directory(tmpPath, false)
+  archive.finalize()
+}
 
 /**
  * cloudInfoController.js
@@ -68,13 +68,13 @@ module.exports = {
    */
   list: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const path = req.query.path;
-      const ans = await ftpService.getList(path);
-      return sendResponse(res, 200, true, "get list succeed", ans);
+      const ftpService = await getFtpService(req)
+      const path = req.query.path
+      const ans = await ftpService.getList(path)
+      return sendResponse(res, 200, true, 'get list succeed', ans)
     } catch (e) {
-      console.log("Get list error!\n", e);
-      return sendResponse(res, 500, false, "get list failed", e);
+      console.log('Get list error!\n', e)
+      return sendResponse(res, 500, false, 'get list failed', e)
     }
   },
 
@@ -83,41 +83,37 @@ module.exports = {
    */
   copy: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const items = req.body.srcPath;
-      const dstPath = req.body.dstPath;
+      const ftpService = await getFtpService(req)
+      const items = req.body.srcPath
+      const dstPath = req.body.dstPath
 
-      const tmpPath = `${req.app.get("public-dir")}/ftp/temp/`;
-      console.log(items);
+      const tmpPath = `${req.app.get('public-dir')}/ftp/temp/`
+      console.log(items)
       if (items.length > 0) {
         for (const srcPath of items) {
-          const filename = path.basename(srcPath.path);
-          console.log("[ftp controller] ===> ", srcPath, tmpPath, filename);
-          await ftpService.download(srcPath, tmpPath);
-          console.log("----> download succeed");
-          const type = srcPath.type;
+          const filename = path.basename(srcPath.path)
+          console.log('[ftp controller] ===> ', srcPath, tmpPath, filename)
+          await ftpService.download(srcPath, tmpPath)
+          console.log('----> download succeed')
+          const type = srcPath.type
 
           if (type === 1)
-            await ftpService.upload(
-              tmpPath + filename,
-              path.join(dstPath, filename),
-              type,
-            );
+            await ftpService.upload(tmpPath + filename, path.join(dstPath, filename), type)
           else
             await ftpService.upload(
               path.join(tmpPath, filename),
               path.join(dstPath, filename),
-              type,
-            );
+              type
+            )
         }
       }
-      return sendResponse(res, 200, true, "copy file succeed", {
+      return sendResponse(res, 200, true, 'copy file succeed', {
         src: items,
         dst: dstPath,
-      });
+      })
     } catch (e) {
-      console.log("copy file error!\n", e);
-      return sendResponse(res, 500, false, "copy file failed", e);
+      console.log('copy file error!\n', e)
+      return sendResponse(res, 500, false, 'copy file failed', e)
     }
   },
 
@@ -126,24 +122,24 @@ module.exports = {
    */
   move: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const srcPath = req.body.srcPath;
-      const dstPath = req.body.dstPath;
-      const tmpPath = `${req.app.get("public-dir")}\\ftp\\upload\\${srcPath}`;
+      const ftpService = await getFtpService(req)
+      const srcPath = req.body.srcPath
+      const dstPath = req.body.dstPath
+      const tmpPath = `${req.app.get('public-dir')}\\ftp\\upload\\${srcPath}`
 
-      await ftpService.download(srcPath, tmpPath);
-      await ftpService.upload(tmpPath, dstPath);
+      await ftpService.download(srcPath, tmpPath)
+      await ftpService.upload(tmpPath, dstPath)
       fs.unlinkSync(tmpPath, {
         force: true,
-      });
-      await ftpService.remove(srcPath);
-      return sendResponse(res, 200, true, "copy file succeed", {
+      })
+      await ftpService.remove(srcPath)
+      return sendResponse(res, 200, true, 'copy file succeed', {
         src: srcPath,
         dst: dstPath,
-      });
+      })
     } catch (e) {
-      console.log("move file error!\n", e);
-      return sendResponse(res, 500, false, "copy file failed", e);
+      console.log('move file error!\n', e)
+      return sendResponse(res, 500, false, 'copy file failed', e)
     }
   },
 
@@ -152,33 +148,29 @@ module.exports = {
    */
   rename: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const srcPath = req.body.srcPath;
-      const dstPath = req.body.dstPath;
+      const ftpService = await getFtpService(req)
+      const srcPath = req.body.srcPath
+      const dstPath = req.body.dstPath
 
-      console.log("[ftp.controller rename] ===> ", srcPath, dstPath);
+      console.log('[ftp.controller rename] ===> ', srcPath, dstPath)
       if (Array.isArray(srcPath)) {
-        const dir = path.dirname(srcPath[0].path);
+        const dir = path.dirname(srcPath[0].path)
         for (const item of srcPath) {
-          const filename = path.basename(item.path);
-          console.log(
-            "[ftp.controller rename] ===> ",
-            item,
-            path.join(dstPath, filename),
-          );
-          await ftpService.rename(item.path, path.join(dstPath, filename));
+          const filename = path.basename(item.path)
+          console.log('[ftp.controller rename] ===> ', item, path.join(dstPath, filename))
+          await ftpService.rename(item.path, path.join(dstPath, filename))
         }
-        const list = await ftpService.getList(dir);
-        return sendResponse(res, 200, true, "rename file succeed", list);
+        const list = await ftpService.getList(dir)
+        return sendResponse(res, 200, true, 'rename file succeed', list)
       } else {
-        const dir = path.dirname(srcPath);
-        await ftpService.rename(srcPath, dstPath);
-        const list = await ftpService.getList(dir);
-        return sendResponse(res, 200, true, "rename file succeed", list);
+        const dir = path.dirname(srcPath)
+        await ftpService.rename(srcPath, dstPath)
+        const list = await ftpService.getList(dir)
+        return sendResponse(res, 200, true, 'rename file succeed', list)
       }
     } catch (e) {
-      console.log("rename file error!\n", e);
-      return sendResponse(res, 500, false, "rename file failed", e);
+      console.log('rename file error!\n', e)
+      return sendResponse(res, 500, false, 'rename file failed', e)
     }
   },
   /**
@@ -186,21 +178,21 @@ module.exports = {
    */
   download: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const items = req.body.srcPath;
-      const target = req.body.dstPath;
-      const tmpPath = `${req.app.get("public-dir")}/ftp/temp/${target}/`;
-      const dstPath = `${req.app.get("public-dir")}/ftp/upload/`;
+      const ftpService = await getFtpService(req)
+      const items = req.body.srcPath
+      const target = req.body.dstPath
+      const tmpPath = `${req.app.get('public-dir')}/ftp/temp/${target}/`
+      const dstPath = `${req.app.get('public-dir')}/ftp/upload/`
       if (items.length > 0) {
         for (const item of items) {
-          await ftpService.download(item, tmpPath);
+          await ftpService.download(item, tmpPath)
         }
       }
-      await archive(dstPath, tmpPath, target);
-      return sendResponse(res, 200, true, "download file succeed", items);
+      await archive(dstPath, tmpPath, target)
+      return sendResponse(res, 200, true, 'download file succeed', items)
     } catch (e) {
-      console.log("download file error!\n", e);
-      return sendResponse(res, 500, false, "download file failed", e);
+      console.log('download file error!\n', e)
+      return sendResponse(res, 500, false, 'download file failed', e)
     }
   },
 
@@ -209,24 +201,23 @@ module.exports = {
    */
   upload: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const srcPath = req.body.srcPath;
-      const uploaded_file = req.files.file;
-      const uploadPath =
-        req.app.get("public-dir") + "/ftp/upload/" + uploaded_file.name;
-      await uploaded_file.mv(uploadPath);
-      await ftpService.upload(uploadPath, srcPath);
+      const ftpService = await getFtpService(req)
+      const srcPath = req.body.srcPath
+      const uploaded_file = req.files.file
+      const uploadPath = req.app.get('public-dir') + '/ftp/upload/' + uploaded_file.name
+      await uploaded_file.mv(uploadPath)
+      await ftpService.upload(uploadPath, srcPath)
 
       fs.unlinkSync(uploadPath, {
         force: true,
-      });
-      return sendResponse(res, 200, true, "upload file succeed", {
+      })
+      return sendResponse(res, 200, true, 'upload file succeed', {
         uploadPath: uploadPath,
         name: req.files.file.name,
-      });
+      })
     } catch (e) {
-      console.log("upload file error!\n", e);
-      return sendResponse(res, 500, false, "upload file failed", e);
+      console.log('upload file error!\n', e)
+      return sendResponse(res, 500, false, 'upload file failed', e)
     }
   },
 
@@ -235,18 +226,18 @@ module.exports = {
    */
   remove: async (req, res) => {
     try {
-      const ftpService = await getFtpService(req);
-      const items = req.body.path;
+      const ftpService = await getFtpService(req)
+      const items = req.body.path
       if (items.length > 0) {
         for (const item of items) {
-          await ftpService.remove(item);
+          await ftpService.remove(item)
         }
-        const list = await ftpService.getList(path.dirname(items[0].path));
-        return sendResponse(res, 200, true, "remove file succeed", list);
+        const list = await ftpService.getList(path.dirname(items[0].path))
+        return sendResponse(res, 200, true, 'remove file succeed', list)
       }
     } catch (e) {
-      console.log("remove file error!\n", e);
-      return sendResponse(res, 500, false, "remove file failed", e);
+      console.log('remove file error!\n', e)
+      return sendResponse(res, 500, false, 'remove file failed', e)
     }
   },
-};
+}
--- services/CloudStorage/FTPService.js
+++ services/CloudStorage/FTPService.js
@@ -1,150 +1,146 @@
 /** @format */
-const ftp = require("basic-ftp");
-const path = require("path");
-const fs = require("fs");
+const ftp = require('basic-ftp')
+const path = require('path')
+const fs = require('fs')
 
 class FTPService {
   constructor() {
-    this.client = {};
-    this.user = null;
+    this.client = {}
+    this.user = null
   }
 
+  getClient = () => {}
+
   checkValidPath = (filePath) => {
-    return filePath === path.basename(filePath);
-  };
+    return filePath === path.basename(filePath)
+  }
   checkDirectoryExist = (filePath) => {
-    return fs.existsSync(filePath);
-  };
+    return fs.existsSync(filePath)
+  }
 
   checkDirectory = async (filePath) => {
-    return (
-      (await this.checkDirectoryExist(filePath)) &&
-      (await fs.stat(filePath).isDirectory())
-    );
-  };
+    return (await this.checkDirectoryExist(filePath)) && (await fs.stat(filePath).isDirectory())
+  }
 
   checkFile = async (filePath) => {
-    return (
-      (await this.checkDirectoryExist(filePath)) &&
-      (await fs.statSync(filePath).isFile())
-    );
-  };
+    return (await this.checkDirectoryExist(filePath)) && (await fs.statSync(filePath).isFile())
+  }
 
   ensureDirectoryExistence = (filePath) => {
-    const dirname = path.dirname(filePath);
-    if (this.checkDirectoryExist(dirname)) return true;
-    this.ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
-  };
+    const dirname = path.dirname(filePath)
+    if (this.checkDirectoryExist(dirname)) return true
+    this.ensureDirectoryExistence(dirname)
+    fs.mkdirSync(dirname)
+  }
 
   async cd(path) {
     try {
       // if(this.checkDirectory(path) === false) throw "such directory not exist";
-      await this.client.cd(path);
+      await this.client.cd(path)
     } catch (error) {
-      console.error("cd path error!\n", error);
-      throw error;
+      console.error('cd path error!\n', error)
+      throw error
     }
   }
 
-  async loginServer(host, user, password) {
+  async getService(host, user, password) {
     try {
       const newUser = {
         host: host,
         user: user,
         password: password,
         // secure: true,
-      };
+      }
 
-      if (JSON.stringify(this.user) !== JSON.stringify(newUser)) {
-        const client = new ftp.Client();
+      if (this.user == null) {
+        const client = new ftp.Client()
         // client.ftp.verbose = true;
-        await client.access(newUser);
-        this.client = client;
-        this.user = newUser;
+        await client.access(newUser)
+        this.client = client
+        this.user = newUser
+      }
+      if (this.client == null) {
+        throw 'get ftp client error'
       }
     } catch (error) {
-      console.error("login server error!\n", error);
-      throw error;
+      console.error('login server error!\n', error)
+      throw error
     }
   }
   async getList(path) {
     try {
-      return await this.client.list(path);
+      return await this.client.list(path)
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 
   async rename(srcPath, dstName) {
     try {
-      const srcBaseName = path.basename(srcPath);
-      const srcDirName = path.dirname(srcPath);
+      const srcBaseName = path.basename(srcPath)
+      const srcDirName = path.dirname(srcPath)
 
-      await this.cd(srcDirName);
+      await this.cd(srcDirName)
 
-      return await this.client.rename(srcBaseName, dstName);
+      return await this.client.rename(srcBaseName, dstName)
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 
   async remove(item) {
     try {
-      const dstPath = item.path;
-      const type = item.type;
+      const dstPath = item.path
+      const type = item.type
 
-      const pathBaseName = path.basename(dstPath);
-      const pathDirName = path.dirname(dstPath);
+      const pathBaseName = path.basename(dstPath)
+      const pathDirName = path.dirname(dstPath)
 
-      await this.cd(pathDirName);
+      await this.cd(pathDirName)
       return type === 1
         ? await this.client.remove(pathBaseName)
-        : await this.client.removeDir(pathBaseName);
+        : await this.client.removeDir(pathBaseName)
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 
   async download(item, dir) {
     try {
-      const srcPath = item.path;
-      const type = item.type;
+      const srcPath = item.path
+      const type = item.type
 
-      const srcBaseName = path.basename(srcPath);
-      const srcDirName = path.dirname(srcPath);
+      const srcBaseName = path.basename(srcPath)
+      const srcDirName = path.dirname(srcPath)
 
-      const dstPath = dir + srcBaseName;
-      await this.cd(srcDirName);
-      this.ensureDirectoryExistence(dstPath);
-      console.log("[FTPService] ====>", dstPath, type);
+      const dstPath = dir + srcBaseName
+      await this.cd(srcDirName)
+      this.ensureDirectoryExistence(dstPath)
+      console.log('[FTPService] ====>', dstPath, type)
 
       return type === 1
-        ? await this.client.downloadTo(
-            fs.createWriteStream(dstPath),
-            srcBaseName,
-          )
-        : await this.client.downloadToDir(dstPath, srcBaseName);
+        ? await this.client.downloadTo(fs.createWriteStream(dstPath), srcBaseName)
+        : await this.client.downloadToDir(dstPath, srcBaseName)
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 
   async upload(srcPath, dstPath, type = 1) {
     try {
-      console.log("[FTPService upload] ===> ", srcPath, dstPath);
-      const dstDirName = path.dirname(dstPath);
-      await this.client.ensureDir(dstDirName);
-      await this.cd(dstDirName);
+      console.log('[FTPService upload] ===> ', srcPath, dstPath)
+      const dstDirName = path.dirname(dstPath)
+      await this.client.ensureDir(dstDirName)
+      await this.cd(dstDirName)
       if (type === 1) {
-        return await this.client.uploadFrom(srcPath, dstPath);
+        return await this.client.uploadFrom(srcPath, dstPath)
       } else {
-        return await this.client.uploadFromDir(srcPath, dstPath);
+        return await this.client.uploadFromDir(srcPath, dstPath)
       }
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 }
 
-module.exports = FTPService;
+module.exports = FTPService
