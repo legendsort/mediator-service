f38778b823bb589b2b1fbb3dbdec148ad03228c3
LuckyZacha <zachatatem@gmail.com>
2022-07-19 10:01:16 +0800

    Fix: browser settime out 3 mins

--- services/Browser/PageEvent.js
+++ services/Browser/PageEvent.js
@@ -122,9 +122,9 @@ const pageEvent = async (page, socket, socketHelper) => {
   // Emitted when a frame within the page is navigated to a new URL
   page.on('framenavigated', async (frame) => {
     console.log('==========>frame navigated ', frame.url())
-    if (frame.url() === 'chrome-error://chromewebdata/') {
-      socketHelper.sendFailureMessage('Internet connection error')
-    }
+    // if (frame.url() === 'chrome-error://chromewebdata/') {
+    //   socketHelper.sendM('Internet connection error')
+    // }
     try {
       socketHelper.sendMessage('status', 'loaded')
 
